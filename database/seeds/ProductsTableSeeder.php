f0c3f11189c6109f5077aea6bcdb9105d815d362
Ryan Tillaman <ryanbuenaventura321@gmail.com>
2020-06-29 21:41:39 +0800

    ext gd added

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -11,6 +11,26 @@ class ProductsTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(Product::class, 20)->create();
+
+
+        DB::table('products')->insert([
+            'name' => "Native chicken", //iya name
+            'desc' => "Nice chicken", //iya description
+            'price' => 99.79, // it iya price
+            'qty' => 100, // kun pira tat stock
+            'type' => 0, //0 for livestock , 1 for agri-farm
+            'img'=>'1.jpeg', //ig sunod sunod la, 1,2,3,4,5 sugad
+            'created_at'=>'2020-06-29 11:45:26', // Date la ini
+        ]);
+        DB::table('products')->insert([
+            'name' => "Malunggay", 
+            'desc' => "Nice malunggay", 
+            'price' => 99.79, 
+            'qty' => 100, 
+            'type' => 1, 
+            'img'=>'2.jpeg', 
+            'created_at'=>'2020-06-29 11:45:26', 
+        ]);
+       
     }
 }
