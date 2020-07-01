2d96ba633252335cd4c6405c9338f6f9df2c96b2
Ryan Tillaman <ryanbuenaventura321@gmail.com>
2020-07-01 17:24:04 +0800

    added logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link rel="icon" href="{{ asset('img/hen.svg')}}">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Farm') }}</title>
+    <title>Farmerce</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
